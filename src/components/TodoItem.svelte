f9b386fe2c67ee433d2ff460bda6b834b849a3bb
ThiccPan <ihilmiderian@gmail.com>
2023-07-30 04:05:23 +0700

    feat: todo item component

---
+++ src/components/TodoItem.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import type { Todo } from '../global';
+
+	const dispatch = createEventDispatcher();
+	export let todoData: Todo;
+</script>
+
+<div>
+	{todoData.task}
+	<button on:click={() => dispatch('delete')}>delete</button>
+</div>
