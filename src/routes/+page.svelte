87484d348222989a9e75a2e815cdd39792cedd15
ThiccPan <ihilmiderian@gmail.com>
2023-07-30 02:42:32 +0700

    feat: test simple todo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { todoStore } from '../store/store';
+
+	let todoList: String[] = [];
+	let inputTodo: string = '';
+
+	function addTodo() {
+		todoList.push(inputTodo);
+		todoList = todoList;
+		console.log(todoList);
+	}
+</script>
+
+<h1>Simple todo app</h1>
+<form method="post" on:submit|preventDefault={() => addTodo()}>
+	<input type="text" bind:value={inputTodo} />
+	<button type="submit">add</button>
+</form>
+
+<h2>List of todo</h2>
+<ul>
+	{#each todoList as todo}
+		<li>
+			{todo}
+		</li>
+	{/each}
+</ul>
