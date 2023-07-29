1d97c1f6ae3f0948793945106777e39431f18427
ThiccPan <ihilmiderian@gmail.com>
2023-07-30 04:06:25 +0700

    feat: refactor todo type and add delete todo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,31 @@
 <script lang="ts">
+	import TodoItem from '../components/TodoItem.svelte';
+	import type { Todo } from '../global';
 	import { todoStore } from '../store/store';
 
-	let todoList: String[] = [];
-	let inputTodo: string = '';
+	let todoList: Todo[] = [];
+	let currId: number = 0;
+	let inputTask: string = '';
 
 	function addTodo() {
-		todoList.push(inputTodo);
+		todoList.push({
+			task: inputTask,
+			id: currId.toString()
+		});
+		currId++;
 		todoList = todoList;
 		console.log(todoList);
+	}
+
+	function deleteTodo(id: string) {
+		console.log(id);
+		todoList = todoList.filter((todo) => todo.id !== id);
 	}
 </script>
 
 <h1>Simple todo app</h1>
 <form method="post" on:submit|preventDefault={() => addTodo()}>
-	<input type="text" bind:value={inputTodo} />
+	<input type="text" bind:value={inputTask} />
 	<button type="submit">add</button>
 </form>
 
@@ -21,7 +33,7 @@
 <ul>
 	{#each todoList as todo}
 		<li>
-			{todo}
+			<TodoItem todoData={todo} on:delete={() => deleteTodo(todo.id)} />
 		</li>
 	{/each}
 </ul>
