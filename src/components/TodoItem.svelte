a208c50c60f79e263324310ec1c6085861262ab6
ThiccPan <ihilmiderian@gmail.com>
2023-07-30 19:13:47 +0700

    feat: update task

--- src/components/TodoItem.svelte
+++ src/components/TodoItem.svelte
@@ -5,20 +5,45 @@
 	const dispatch = createEventDispatcher();
 	export let todoData: Todo;
 
-	function doneAction() {
+	function updateDone() {
 		todoData.isDone = !todoData.isDone;
 		dispatch('done');
 	}
+
+	let updateTaskInput: string = '';
+	function updateTask() {
+		console.log('task updated');
+		todoData.task = updateTaskInput;
+		updateTaskInput = '';
+		if (todoData.isDone) {
+			todoData.isDone = false;
+		}
+	}
 </script>
 
-<div>
+<div class="mb-2">
 	<span class={todoData.isDone === true ? 'crossed' : ''}>{todoData.task}</span>
-	<button on:click={() => doneAction()}>done</button>
+	<br />
+	<button on:click={() => updateDone()}>done</button>
 	<button on:click={() => dispatch('delete')}>delete</button>
+</div>
+<div class="mb-3">
+	<form action="post" on:submit|preventDefault={updateTask}>
+		<input type="text" name="update" id="update" bind:value={updateTaskInput} />
+		<button type="submit">update</button>
+	</form>
 </div>
 
 <style>
 	.crossed {
 		text-decoration: line-through;
+	}
+
+	.mb-2 {
+		margin-bottom: 8px;
+	}
+
+	.mb-3 {
+		margin-bottom: 12px;
 	}
 </style>
